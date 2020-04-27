9c9b0cf5e4bd61ad410690e1eac6e35dc8fdc027
MerazAli <61820721+MerazAli@users.noreply.github.com>
2020-04-27 23:34:26 +0530

    Update guesture.py

--- guesture.py
+++ guesture.py
@@ -28,78 +28,7 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):   # break when q is pressed
             break
     else:
-        break
-
-
-
-
-
-"""#import cv2  
-#import numpy as np
-   
-
-        face_classifier=cv2.CascadeClassifier('C:/Users/des/Demo-copy/Documents/harcas file/haarcascade_frontalface_default.xml')
-                                       # Cascade classifier used to extract all face
-                              
-   #  extract face feature
-
-def face_extractor(img):
-
-    #gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #faces=face_classifier.detectMultiScale(gray,1.3,5)
-
-    if face is ():
-        return None
-
-    for (x,y,w,h) in face:
-        cropped_face=img[y:y+h, x:x+w]
-
-    return cropped_face
-
-
-
-
-#cap=cv2.VideoCapture(0)
-count=0
-
-while True:
-    ret, face=cap.read()
-    if face_extractor(frame) is not None:
-        count+=1
-        face=cv2.resize(face_extractor(frame),(400,400))                # adjust ur face as required to camera
-        face=cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)                                                                                                                         
-
-
-        file_name_path='C:/Users/des/Demo-copy/Documents/opencv/Training image/meraz'+str(count)+'.jpg'
-        cv2.imwrite(file_name_path,face)
-
-
-        cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)        # used to count no.  of photos
-        cv2.imshow('Face cropper', face)
-
-
-
-    else:
-        print("Face not found")  """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        break                                                                                                              
 
 cap.release()
 cv2.destroyAllWindows()    
