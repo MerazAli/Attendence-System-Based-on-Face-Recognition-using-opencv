c2ee727e29a8ef3a8aa86b0143d0508cdb54c7bc
MerazAli <61820721+MerazAli@users.noreply.github.com>
2020-07-02 13:58:03 +0530

    Create database_orm2.py

---
+++ database_orm2.py
@@ -0,0 +1,30 @@
+
+            #  sqlite3 test_db.db    for compile
+
+
+import sqlalchemy
+
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__="products"
+    id=Column(Integer,primary_key=True)
+    name=Column(String)
+    
+    college=Column(String)
+
+
+class Users(Base):
+    __tablename__="users"
+    id = Column(Integer,primary_key=True)
+    name = Column(String)
+    college = Column(String)
+
+
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///db.sqlite3')
+    Base.metadata.create_all(engine)
