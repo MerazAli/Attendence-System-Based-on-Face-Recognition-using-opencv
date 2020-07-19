b55bcf70858eedfa2820a38d9b90ee2d4ac78fb7
MerazAli <61820721+MerazAli@users.noreply.github.com>
2020-07-19 13:30:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,8 +111,8 @@ def trainer():
 def attendance_view():
     data = pd.read_sql('attendance',engine)
     today =date.today()
-    today_result = Attendance.query.filter(date=today)
-    return render_template('attendance.html',data = data.to_html(classes=('table','table-hovered','table-sm','table-responsive'),max_rows=None,max_cols=None,table_id='attendance',index=False,),today_result=today_result)
+    today_result = sess.query(Attendance).filter_by(date=today).all()
+    return render_template('attendance.html',data = data.to_html(classes=(),max_rows=None,max_cols=None,table_id='attendance',index=False,),today_result=today_result)
 
 
 if __name__=='__main__':
